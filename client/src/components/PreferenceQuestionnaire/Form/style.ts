919e0e5e3759e9b2c0b569755cc35b5baf3b5f6a
jjonnyzz <xd.JZahavi@idf.il>
2024-07-20 12:45:24 +0300

    chore: Remove unused PreferenceQuestionnaire components and styles

--- client/src/components/PreferenceQuestionnaire/Form/style.ts
+++
@@ -1,8 +0,0 @@
-import { Button, styled } from '@mui/material';
-
-export const FormActionButton = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.info.main,
-  color: theme.palette.primary.contrastText,
-  width: 'fit-content',
-  height: 'fit-content',
-}));
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,5 +2,4 @@
 export * from './Home';
 export * from './HOC';
 export * from './Navbar';
-export * from './PreferenceQuestionnaire';
 export * from './TrainersImages';
