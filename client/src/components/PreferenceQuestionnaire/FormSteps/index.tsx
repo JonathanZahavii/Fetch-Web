d1a6133b09b18fe9585419bc1a9ae2b0b176cb53
JonathanZahavii <111435861+JonathanZahavii@users.noreply.github.com>
2024-07-20 12:46:09 +0300

    chore: Remove unused PreferenceQuestionnaire components and styles (#2)
    * chore: Remove unused PreferenceQuestionnaire components and styles
    
    * Merge remote-tracking branch 'origin/main' into delete-train-code

--- client/src/components/PreferenceQuestionnaire/FormSteps/index.tsx
+++
@@ -1,7 +0,0 @@
-export { AboutYourselfForm } from './AboutYourselfForm';
-export { PhysicalDataForm } from './PhysicalDataForm';
-export { ActivityLevelForm } from './ActivityLevelForm';
-export { GoalsForm } from './GoalsForm';
-export { DaysForm } from './DaysForm';
-export { WorkoutsForm } from './WorkoutsForm';
-export { WorkoutDurationForm } from './WorkoutDurationForm';
