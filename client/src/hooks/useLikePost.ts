96a6445bb65411d1cdf64aee10f87e2e2c72c5dd
JonathanZahavii <111435861+JonathanZahavii@users.noreply.github.com>
2024-07-27 04:09:28 +0300

    Like (#20)

---
+++ client/src/hooks/useLikePost.ts
@@ -0,0 +1,23 @@
+import { ErrorFunction } from '@shared/types/errorFunction.type';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { USE_GET_POSTS_KEY } from './post/useGetPosts';
+
+type UseLikeProps = {
+  postId: string;
+  userId: string;
+};
+const likePost = async ({ postId, userId }: UseLikeProps) => {
+  console.log('like post: ' + postId + ' by user: ' + userId);
+  // await api.post(`/posts/${postId}/like`, { userId });
+};
+
+export const useLikePost = (onError: ErrorFunction) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ postId, userId }: UseLikeProps) => likePost({ postId, userId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [USE_GET_POSTS_KEY] });
+    },
+    onError,
+  });
+};
