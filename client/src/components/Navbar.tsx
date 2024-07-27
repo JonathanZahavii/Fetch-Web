c8ee642b09fdcaf70c4f1dc452a16a9b3b4d37db
jjonnyzz <xd.JZahavi@idf.il>
2024-07-27 04:38:40 +0300

    Refactor navbar to grid

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import AuthContext from '@/contexts/AuthContext';
 import useDialog from '@/hooks/useDialog';
 import AddPost from '@/pages/AddPost/AddPost';
 import { HOME_URL, LOGIN_URL, PROFILE_URL, SIGNUP_URL } from '@/router/router.const';
-import { AppBar, Avatar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, Avatar, Box, Button, Grid, IconButton, Toolbar, Typography } from '@mui/material';
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LogoutButton from './LogoutButton';
@@ -13,65 +13,77 @@ const Navbar: React.FC = () => {
   const { currentUser } = useContext(AuthContext);
   const { isOpen, close, open } = useDialog();
 
+  const handleNavigateProfile = () => {
+    navigate(PROFILE_URL);
+  };
+
   return (
     <AppBar position="sticky">
       <Toolbar>
-        {/* Left Side: Logo and Two Buttons */}
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton onClick={() => navigate(HOME_URL)}>
-            <Box component="img" src={AppLogo} sx={{ width: '6vw' }} />
-          </IconButton>
-          {currentUser && (
-            <Button color="inherit" onClick={open}>
-              Add Post
-            </Button>
-          )}
-          {/* <Button color="inherit" onClick={() => navigate(WORKOUTS_URL)}>
-            WORKOUTS
-          </Button> */}
-        </Box>
+        <Grid container sx={{ alignItems: 'center' }}>
+          <Grid item container sx={{ alignItems: 'center' }} xs={4}>
+            <Grid item>
+              <IconButton onClick={() => navigate(HOME_URL)}>
+                <Box component="img" src={AppLogo} sx={{ width: '6vw' }} />
+              </IconButton>
+            </Grid>
+            {currentUser && (
+              <Grid item>
+                <Button color="inherit" onClick={open}>
+                  Add Post
+                </Button>
+              </Grid>
+            )}
+          </Grid>
 
-        {/* Spacer to push content to the right */}
-        <Box sx={{ flexGrow: 1 }} />
-
-        {/* Right Side: Conditional Rendering */}
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5vw' }}>
-          {currentUser ? (
-            <>
-              <Button onClick={() => navigate(PROFILE_URL)}>
-                {currentUser.photoURL ? (
-                  <Box
-                    component="img"
-                    src={currentUser.photoURL}
-                    sx={{ width: '6vw' }}
-                    onClick={() => navigate(PROFILE_URL)}
-                  />
-                ) : (
-                  <Avatar src="" onClick={() => navigate(PROFILE_URL)} />
-                )}
-              </Button>
-              <Typography variant="body1" color="inherit">
-                Hello, {currentUser.name}
-              </Typography>
-              <LogoutButton />
-            </>
-          ) : (
-            <>
-              <Button
-                sx={{ backgroundColor: 'info.main', borderRadius: '15px', p: '6px 12px' }}
-                onClick={() => navigate(LOGIN_URL)}
-              >
-                SIGN IN
-              </Button>
-              <Button
-                sx={{ backgroundColor: 'primary.light', borderRadius: '15px', p: '6px 12px' }}
-                onClick={() => navigate(SIGNUP_URL)}
-              >
-                SIGN UP
-              </Button>
-            </>
-          )}
-        </Box>
+          <Grid item container sx={{ alignItems: 'center', justifyContent: 'flex-end' }} xs={8}>
+            {currentUser ? (
+              <>
+                <Grid item>
+                  <Button onClick={handleNavigateProfile}>
+                    {currentUser.photoURL ? (
+                      <Box
+                        component="img"
+                        src={currentUser.photoURL}
+                        sx={{ width: '6vw' }}
+                        onClick={handleNavigateProfile}
+                      />
+                    ) : (
+                      <Avatar src="" onClick={handleNavigateProfile} />
+                    )}
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body1" color="inherit">
+                    Hello, {currentUser.name}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <LogoutButton />
+                </Grid>
+              </>
+            ) : (
+              <>
+                <Grid item>
+                  <Button
+                    sx={{ backgroundColor: 'info.main', borderRadius: '15px', p: '6px 12px' }}
+                    onClick={() => navigate(LOGIN_URL)}
+                  >
+                    SIGN IN
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    sx={{ backgroundColor: 'primary.light', borderRadius: '15px', p: '6px 12px' }}
+                    onClick={() => navigate(SIGNUP_URL)}
+                  >
+                    SIGN UP
+                  </Button>
+                </Grid>
+              </>
+            )}
+          </Grid>
+        </Grid>
       </Toolbar>
       <AddPost isOpen={isOpen} close={close} />
     </AppBar>
