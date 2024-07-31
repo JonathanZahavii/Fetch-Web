3ae3104acdfd92ff9c5e14983e4b6650012ca374
noypollak <noypollak@gmail.com>
2024-07-31 22:34:42 +0300

    add uuid default value

--- server/src/models/postModel.ts
+++ server/src/models/postModel.ts
@@ -1,5 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
 import { Post as PostType } from '@shared/types/post.type';
+import { v4 as uuidv4 } from 'uuid';
 
 interface IPost extends Document, Omit<PostType, 'image' | 'user' | 'comments'> {
   user: string;
@@ -12,7 +13,7 @@ const PostSchema: Schema = new Schema({
   image: { type: String, required: true },
   caption: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  uuid: { type: String, required: true, unique: true },
+  uuid: { type: String, required: true, default: uuidv4, unique: true },
   user: { type: String, required: true },
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
   likes: { type: Number, default: 0 },
