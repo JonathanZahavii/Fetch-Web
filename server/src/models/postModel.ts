7ce5a02faadfd36b93efe0baee9c394b0b6644aa
noypollak <noypollak@gmail.com>
2024-07-31 22:10:17 +0300

    adjust code to client

--- server/src/models/postModel.ts
+++ server/src/models/postModel.ts
@@ -1,11 +1,11 @@
 import mongoose, { Document, Schema } from 'mongoose';
-import { Post as PostType} from '@shared/types/post.type';
+import { Post as PostType, upsertPost } from '@shared/types/post.type';
 
 interface IPost extends Document, Omit<PostType, 'image' | 'user' | 'comments'> {
   user: string;
   uuid: string;
   caption: string;
-  image?: string;
+  image: string;
 }
 
 const PostSchema: Schema = new Schema({
@@ -13,7 +13,7 @@ const PostSchema: Schema = new Schema({
   caption: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   uuid: { type: String, required: true, unique: true },
-  user: { type: String, required: true }, // Assuming user ID or a reference to the user
+  user: { type: String, required: true },
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
   likes: { type: Number, default: 0 },
   location: { type: String },
--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import { addPost, editPost, deletePost, getPosts, likePost, upsertPost } from '../controllers/postController';
+import { upsertPost, deletePost, getPosts, likePost } from '../controllers/postController';
 
 const router = Router();
 
 router.post('/', upsertPost);
-router.put('/edit/:id', editPost);
 router.delete('/delete/:id', deletePost);
 router.get('/', getPosts);
 router.put('/like/:id', likePost);
