674b5c90843fbcce0b426608fcff821444796a97
JonathanZahavii <111435861+JonathanZahavii@users.noreply.github.com>
2024-08-01 05:17:53 +0300

    Update user w/o photo (#39)

--- client/src/hooks/user/useUpdateUser.ts
+++ client/src/hooks/user/useUpdateUser.ts
@@ -1,24 +1,18 @@
-import { auth } from '@/configs/firebaseConfig';
+import api from '@/api/api';
 import { ErrorFunction } from '@shared/types/errorFunction.type';
 import { User } from '@shared/types/user.type';
 import { useMutation } from '@tanstack/react-query';
-import { updateProfile } from 'firebase/auth';
 
 export const updateUser = async (user: User) => {
-  // return await Api.put('/auth/user/', { user });
-  const { currentUser } = auth;
-  currentUser &&
-    (await updateProfile(currentUser, {
-      displayName: user?.name,
-      photoURL: user?.photoURL,
-    }));
-  return user;
+  return (await api.put('/auth/user/', { user })).data;
 };
 
 export const useUpdateUser = (onSuccess: (data: User) => void, onError: ErrorFunction) => {
   return useMutation({
-    mutationFn: updateUser,
-    onSuccess,
+    mutationFn: (user: User) => updateUser(user),
+    onSuccess: (data: User) => {
+      onSuccess(data);
+    },
     onError,
   });
 };
