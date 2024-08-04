f1fca7fd4e9996896a550cb64c6c24614436944e
jjonnyzz <xd.JZahavi@idf.il>
2024-08-05 00:57:24 +0300

    Set user column to type User

--- server/src/models/postModel.ts
+++ server/src/models/postModel.ts
@@ -1,12 +1,14 @@
+import { Comment, Post as PostType } from '@shared/types/post.type';
+import { User } from '@shared/types/user.type';
 import mongoose, { Document, Schema } from 'mongoose';
-import { Post as PostType } from '@shared/types/post.type';
 import { v4 as uuidv4 } from 'uuid';
 
-interface IPost extends Document, Omit<PostType, 'image' | 'user' | 'comments'> {
-  user: string;
+interface IPost extends Document, PostType {
+  user: User;
   _id: string;
   caption: string;
   image: string;
+  comments: Comment[];
 }
 
 const PostSchema: Schema = new Schema({
@@ -14,7 +16,7 @@ const PostSchema: Schema = new Schema({
   caption: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   _id: { type: String, required: true, default: uuidv4(), unique: true },
-  user: { type: String, required: true },
+  user: { type: [Schema.Types.ObjectId], required: true, ref: 'User' },
   comments: { type: [Schema.Types.ObjectId], default: [], ref: 'Comment' },
   likes: { type: Number, default: 0 },
   location: { type: String },
