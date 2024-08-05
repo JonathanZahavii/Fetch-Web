d7ef81bd3be84d13d223d41300dededeb9aa6920
jjonnyzz <xd.JZahavi@idf.il>
2024-08-06 00:32:05 +0300

    Refetch posts update profile

--- client/src/hooks/user/useUpdateUser.ts
+++ client/src/hooks/user/useUpdateUser.ts
@@ -1,7 +1,8 @@
 import api from '@/api/api';
 import { ErrorFunction } from '@shared/types/errorFunction.type';
 import { UpdateUser, User } from '@shared/types/user.type';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { USE_GET_POSTS_KEY } from '../post/useGetPosts';
 
 export const updateUser = async (user: UpdateUser) => {
   return (
@@ -14,9 +15,11 @@ export const updateUser = async (user: UpdateUser) => {
 };
 
 export const useUpdateUser = (onSuccess: (data: User) => void, onError: ErrorFunction) => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (user: UpdateUser) => updateUser(user),
     onSuccess: (data: User) => {
+      queryClient.invalidateQueries({ queryKey: [USE_GET_POSTS_KEY] });
       onSuccess(data);
     },
     onError,
