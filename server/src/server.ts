e23e7c155644650e98e975e0e12e7a6170652488
noypollak <noypollak@gmail.com>
2024-08-06 02:33:52 +0300

    add https

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,23 @@
 import app from './app';
 import logger from './utils/logger.util';
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  logger.info(`Server is running on port ${port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  console.log('development');
+  http.createServer(app).listen(process.env.PORT, () => {
+    logger.info(`Server is running on port ${process.env.PORT}`);
+  });
+} else {
+  console.log('PRODUCTION');
+  const options2 = {
+    key: fs.readFileSync('../client-key.pem'),
+    cert: fs.readFileSync('../client-cert.pem')
+  };
+  https.createServer(options2, app).listen(process.env.HTTPS_PORT, () => {
+    logger.info(`Server is running on port ${process.env.PORT}`);
+  });
+}
